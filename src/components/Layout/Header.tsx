040fa503848e1443f122fb906fe816c94f53f395
mananjay82i <mananjay.kumar@inviz.ai>
2023-04-29 13:44:12 +0530

    checking my updates are deployed

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -42,7 +42,7 @@ export const Header = ({ user }: { user: any }) => {
           }}
           onClick={() => navigate(ROOT)}
         >
-          Home
+          Social Media App
         </Typography>
         <Button
           sx={{
